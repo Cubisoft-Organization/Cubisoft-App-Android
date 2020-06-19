1af74d8a0d93c045c90d82e0c8bea2f931b6bafd
misael herrera <misaelhb.21@gmail.com>
2020-06-19 11:14:36 -0500

    feature reserve avance

---
+++ CubisoftApp/app/src/main/java/pe/edu/upc/cubisoftapp/ReserveCubiculoActivity.kt
@@ -0,0 +1,12 @@
+package pe.edu.upc.cubisoftapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReserveCubiculoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserve_cubiculo)
+    }
+}
